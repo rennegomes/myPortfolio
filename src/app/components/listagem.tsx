e8a6f91a86ba8b23e4fc208bb78662886c824afd
rennegomes <renne.lace082@gmail.com>
2025-03-11 14:44:27 -0300

    :ok_hand: style: Listagem padrão finalizado

--- src/app/components/listagem.tsx
+++ src/app/components/listagem.tsx
@@ -1,20 +1,130 @@
 export default function Listagem(){
     return(
-        <div className="bg-zinc-950 p-5 my-5 rounded-lg">
-            <div className="flex flex-col gap-5">
-                <strong className="text-lg">Habilidades Front-End</strong>
-                <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 items-start">
+        <div>
+            <div className="bg-zinc-950 p-5 my-5 rounded-lg">
+                <div className="flex flex-col gap-5">
+                    <strong className="text-lg">Habilidades Front-End</strong>
+                    <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 items-start">
 
-                    <div className="bg-zinc-800 p-5 rounded-lg hover:bg-[#D8FA00] hover:text-zinc-900 transition-colors duration-200">
-                        <div className="flex items-center gap-5">
-                            <img className="max-w-10 max-h-10 rounded-lg" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg" alt="" />
-                            <div>
-                                <strong>Next.js</strong>
-                                <p className="text-xs font-light">Framework React com maior poder de performance</p>
+                        <div className="bg-zinc-800 p-5 rounded-lg hover:bg-[#D8FA00] hover:text-zinc-900 transition-colors duration-200">
+                            <div className="flex items-center gap-5">
+                                <img className="max-w-10 max-h-10 rounded-lg" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg" alt="" />
+                                <div>
+                                    <strong>Next.js</strong>
+                                    <p className="text-xs font-light">Framework React com maior poder de performance</p>
+                                </div>
                             </div>
                         </div>
+
+                        <div className="bg-zinc-800 p-5 rounded-lg hover:bg-[#D8FA00] hover:text-zinc-900 transition-colors duration-200">
+                            <div className="flex items-center gap-5">
+                                <img className="max-w-10 max-h-10 rounded-lg" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg" alt="" />
+                                <div>
+                                    <strong>React</strong>
+                                    <p className="text-xs font-light">React é uma biblioteca para interfaces de usuário dinâmicas.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-zinc-800 p-5 rounded-lg hover:bg-[#D8FA00] hover:text-zinc-900 transition-colors duration-200">
+                            <div className="flex items-center gap-5">
+                                <img className="max-w-10 max-h-10 rounded-lg" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="" />
+                                <div>
+                                    <strong>JavaScript</strong>
+                                    <p className="text-xs font-light">React é uma biblioteca para interfaces de usuário dinâmicas.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-zinc-800 p-5 rounded-lg hover:bg-[#D8FA00] hover:text-zinc-900 transition-colors duration-200">
+                            <div className="flex items-center gap-5">
+                                <img className="max-w-10 max-h-10 rounded-lg" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="" />
+                                <div>
+                                    <strong>TypeScript</strong>
+                                    <p className="text-xs font-light">TypeScript adiciona tipagem estática ao JavaScript.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-zinc-800 p-5 rounded-lg hover:bg-[#D8FA00] hover:text-zinc-900 transition-colors duration-200">
+                            <div className="flex items-center gap-5">
+                                <img className="max-w-10 max-h-10 rounded-lg" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original.svg" alt="" />
+                                <div>
+                                    <strong>TailwindCSS</strong>
+                                    <p className="text-xs font-light">TailwindCSS é um framework CSS que prioriza a utilidade.</p>
+                                </div>
+                            </div>
+                        </div>
+                        
                     </div>
-                    
+                </div>
+            </div>
+
+            <div className="bg-zinc-950 p-5 my-5 rounded-lg">
+                <div className="flex flex-col gap-5">
+                    <strong className="text-lg">Habilidades Back-End</strong>
+                    <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 items-start">
+
+                        <div className="bg-zinc-800 p-5 rounded-lg hover:bg-[#D8FA00] hover:text-zinc-900 transition-colors duration-200">
+                            <div className="flex items-center gap-5">
+                                <img className="max-w-10 max-h-10 rounded-lg" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg" alt="" />
+                                <div>
+                                    <strong>Node.js</strong>
+                                    <p className="text-xs font-light">Node.js é um ambiente de execução JavaScript rápido no lado do servidor.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-zinc-800 p-5 rounded-lg hover:bg-[#D8FA00] hover:text-zinc-900 transition-colors duration-200">
+                            <div className="flex items-center gap-5">
+                                <img className="max-w-10 max-h-10 rounded-lg" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg" alt="" />
+                                <div>
+                                    <strong>MySQL</strong>
+                                    <p className="text-xs font-light">MySQL é um banco de dados relacional de código aberto que utiliza SQL.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-zinc-800 p-5 rounded-lg hover:bg-[#D8FA00] hover:text-zinc-900 transition-colors duration-200">
+                            <div className="flex items-center gap-5">
+                                <img className="max-w-10 max-h-10 rounded-lg" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg" alt="" />
+                                <div>
+                                    <strong>MongoDB</strong>
+                                    <p className="text-xs font-light">MongoDB é um banco de dados NoSQL com documentos semelhantes a JSON.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className="bg-zinc-950 p-5 my-5 rounded-lg">
+                <div className="flex flex-col gap-5">
+                    <strong className="text-lg">Ferramentas</strong>
+                    <div className="grid grid-cols-1 gap-5 lg:grid-cols-2 items-start">
+
+                        <div className="bg-zinc-800 p-5 rounded-lg hover:bg-[#D8FA00] hover:text-zinc-900 transition-colors duration-200">
+                            <div className="flex items-center gap-5">
+                                <img className="max-w-10 max-h-10 rounded-lg" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg" alt="" />
+                                <div>
+                                    <strong>Figma</strong>
+                                    <p className="text-xs font-light">Figma é uma ferramenta de design UI/UX baseada na nuvem.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-zinc-800 p-5 rounded-lg hover:bg-[#D8FA00] hover:text-zinc-900 transition-colors duration-200">
+                            <div className="flex items-center gap-5">
+                                <img className="max-w-10 max-h-10 rounded-lg" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg" alt="" />
+                                <div>
+                                    <strong>Visual Studio Code</strong>
+                                    <p className="text-xs font-light">Visual Studio Code é um editor de código rápido e de código aberto.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
